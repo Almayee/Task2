2210e545d28e2a8f62dab222616f324e125024ac
Memmedli9 <jeyncorc@gmail.com>
2024-03-11 11:01:42 +0400

    task2

---
+++ Tasks/Tasks/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ededin reqemlerinin hasili 
+            //int number =1905;
+            //int hasil = 1;
+            //int a;
+            //while (number != 0)
+            //{
+            //    a = number % 10;
+            //    number /= 10;
+            //    hasil *= a;
+            //}
+            //Console.WriteLine(hasil);
+            #endregion
+
+            #region sade ve ya merekkeb ededler
+            //int eded = 11;
+
+            //if (eded <= 1)
+            //{
+            //    Console.WriteLine("ne sadedir ne murekkeb");
+            //}
+            //else if (eded == 2)
+            //{
+            //    Console.WriteLine("2 ededi sadedir.");
+            //}
+            //else
+            //{
+            //    bool sade = true;
+            //    for (int i = 2; i * i <= eded; i++)
+            //    {
+            //        if (eded % i == 0)
+            //        {
+            //            sade = false;
+            //            break;
+            //        }
+            //    }
+
+            //    if (sade)
+            //    {
+            //        Console.WriteLine($"{eded} ededi sadedir.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{eded} ededi mürekkebdir.");
+            //    }
+            //}
+            #endregion
+
+            #region faktorial
+            //int n = 5;
+            //int faktorial = 1;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    faktorial = faktorial * i;
+            //}
+            //Console.WriteLine(faktorial);
+            #endregion
+
+
+        }
+    }
+}
